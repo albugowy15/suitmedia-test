c59c6a3594bfa392a607348b6f721825ece9db05
albugowy15 <kholidbughowi@gmail.com>
2023-01-14 13:31:43 +0700

    add third library

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,10 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'),],
 }
